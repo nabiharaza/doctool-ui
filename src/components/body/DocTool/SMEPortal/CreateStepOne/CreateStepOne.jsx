984844c20b7406af17d461f78b296e0345805993
neelkirit <neelkirit2@gmail.com>
2020-08-04 19:05:23 -0400

    Template Fetch Call I followed this - https://reactjs.org/docs/faq-ajax.html

--- src/components/body/DocTool/SMEPortal/CreateStepOne/CreateStepOne.jsx
+++ src/components/body/DocTool/SMEPortal/CreateStepOne/CreateStepOne.jsx
@@ -3,28 +3,67 @@ import PageTitle from "../../../../modules/PageTitle/PageTitle";
 
 
 class CreateStepOne extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/users/octocat")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        return (
-            <div className='container'>
-                <PageTitle title={'Fill the details'}/>
-                <div className='row'>
-                    <div className='col-md-4'>
-                        <form>
-                            <div className="form-group">
-                                <select className="custom-select">
-                                    <option selected>Select Document type to create</option>
-                                    <option value="1">Test Documents</option>
-                                </select>
-                            </div>
+        const {error, isLoaded, items} = this.state;
+
+        console.log(items);
+        if (error) {
+            return "LOL"
+        } else {
+            return (
+                <div className='container'>
+                    <PageTitle title={'Fill the details'}/>
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <form>
+                                <div className="form-group">
+                                    {/*<select className="custom-select">*/}
+                                    {/*    <option selected>Select Document type to create</option>*/}
+                                    {/*    <option value="1">Test Documents</option>*/}
+                                    {/*</select>*/}
+                                </div>
 
 
-                            <button type="submit" className="btn btn-primary">Next</button>
-                        </form>
+                                <button type="submit" className="btn btn-primary">Next</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        );
+            );
+        }
+
     }
 }
 

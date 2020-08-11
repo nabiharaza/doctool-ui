ff620afacb8943927723ee9a7a7ba3f19102d92e
nabiharaza <nabiha.raza@hpe.com>
2020-08-11 19:57:53 -0400

    StepOne Created. API call working

--- src/components/body/DocTool/SMEPortal/CreateStepOne/CreateStepOne.jsx
+++ src/components/body/DocTool/SMEPortal/CreateStepOne/CreateStepOne.jsx
@@ -1,31 +1,69 @@
-import React, {Component} from "react";
+import React from "react";
 import PageTitle from "../../../../modules/PageTitle/PageTitle";
 
+export default class CreateStepOne extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
 
-class CreateStepOne extends Component {
+    componentDidMount() {
+        fetch("http://127.0.0.1:5000/api/v1/folders")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        return (
-            <div className='container'>
-                <PageTitle title={'Fill the details'}/>
-                <div className='row'>
-                    <div className='col-md-4'>
-                        <form>
-                            <div className="form-group">
-                                <select className="custom-select">
-                                    <option selected>Select Document type to create</option>
-                                    <option value="1">Test Documents</option>
+        const {error, isLoaded, items} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            let foldersList = Object.keys(items.response).map((k) => {
+                return (
+                    <option key={k}
+                            value={JSON.stringify(items.response[k].folder_id)}>{JSON.stringify(items.response[k].folder_name)}</option>
+                )
+            }, this);
+            return (
+                <div className='container'>
+                    <PageTitle title={'Fill the details'}/>
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <form>
+                                <select>
+                                    {foldersList}
+                                    
                                 </select>
-                            </div>
-
-
-                            <button type="submit" className="btn btn-primary">Next</button>
-                        </form>
+                                
+                                <button type="submit" className="btn btn-primary">Next</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-        );
+            );
+        }
     }
 }
 
-export default CreateStepOne
